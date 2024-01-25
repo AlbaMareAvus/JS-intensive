357352720356430248bdbfea6ca5d701dab0069a
Vladimir Galkin <galkin.ve.home@gmail.com>
2024-01-25 16:49:54 +0300

    add bonus task 1

---
+++ bonus.1-task.js
@@ -0,0 +1,36 @@
+const obj1 = {
+  here: { is: 'on', other: '3' },
+  object: 'Y'
+};
+
+const obj2 = {
+  here: { is: 'on', other: '2' },
+  object: 'Y'
+};
+
+/**
+ * Функция глубокого сравнения двух объектов
+ * @param {Object} obj1 первый объект
+ * @param {Object} obj2 второй объект
+ * @returns {Boolean|String} Результат сравнения (true/false) или сообщение об ошибки
+ */
+const deepEqual = (obj1, obj2) => {
+  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') return 'Переданы не объекты';
+  if (obj1 === null || obj2 === null) return 'Переданы значения null';
+
+  for (let key in obj1) {
+    if (!obj2.hasOwnProperty(key)) return false;
+
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      const res = deepEqual(obj1[key], obj2[key]);
+
+      if (res === false) return false;
+    } else {
+      if (obj1[key] !== obj2[key]) return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(deepEqual(obj1, obj2));
